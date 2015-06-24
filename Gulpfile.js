efebbd7cc8d1bfecb4e703a1823d33b6dfa939cc
Peyman Safay <peyman@locasphere.com>
2015-06-24 12:33:32 -0400

    Updated build process to include sub folders

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,29 +3,37 @@ var sass = require('gulp-sass');
 var neat = require('node-neat').includePaths;
 var autoprefixer = require('gulp-autoprefixer');
 var connect = require('gulp-connect');
+var livereload = require('gulp-livereload');
 
 gulp.task('html', function() {
-	gulp.src('dev/*.html')
-		.pipe(connect.reload());
+	gulp.src('dev/**/*.html')
+		.pipe(livereload());
 })
 
-gulp.task('css', function() {
+gulp.task('sass', function() {
    return gulp.src('dev/style/sass/main.scss')
          .pipe(sass({
             includePaths: ['dev\\style\\sass'].concat(neat)
          }))
          .pipe(autoprefixer('last 10 version'))
          .pipe(gulp.dest('dev/style/'))
-         .pipe(connect.reload());;
+         .pipe(livereload());
 });
 
+gulp.task('css', function() {
+   return gulp.src('dev/**/*.css')
+         .pipe(livereload());
+})
+
 gulp.task('server', function() {
-	connect.server({root: 'dev', livereload:true});
+	connect.server({root: 'dev'});
 });
 
 gulp.task('watch', function() {
-   gulp.watch('dev/style/sass/**/*.scss', ['css']);
-   gulp.watch('dev/*.html', ['html']);
+   livereload.listen();
+   gulp.watch('dev/style/sass/**/*.scss', ['sass']);
+   gulp.watch('dev/saacartfundraiser/**/*.css', ['css']);
+   gulp.watch('dev/**/*.html', ['html']);
 });
 
 gulp.task('default', ['watch', 'server']);
